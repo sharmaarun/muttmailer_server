f78d6474403c08df855691f46c2ab6bdf8ef6504
Arun Sharma <sharma.arun333@gmail.com>
2018-07-12 01:08:39 +0530

    fixing ..

--- server.js
+++ server.js
@@ -15,6 +15,25 @@ const port                  = 8000;
 app.use(bodyParser.urlencoded({extended:true}));
 
 //create/init REST routes
+// Add headers
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://easyfastloans.co:3000');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
 require("./app/routes")(app,{});
 
 
