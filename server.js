b69e3cf6a0f0807e4845cbe8f668263040057099
Arun Sharma <sharma.arun333@gmail.com>
2018-07-12 01:15:16 +0530

    fixing

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 const express               = require("express");
 const mongoClient           = require("mongodb").MongoClient;
 const bodyParser            = require("body-parser");
-
+const cors                  = require("cors");
 const app                   = express();
 
 
@@ -16,24 +16,7 @@ app.use(bodyParser.urlencoded({extended:true}));
 
 //create/init REST routes
 // Add headers
-app.use(function (req, res, next) {
-
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://easyfastloans.co:3000');
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
-    next();
-});
+app.use(cors());
 require("./app/routes")(app,{});
 
 
